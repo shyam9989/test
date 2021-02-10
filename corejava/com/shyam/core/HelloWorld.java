c8a261336bc7fcbed0f9f63a15960b703be5ed21
shyam pilli <spil3003@7-11.com>
2021-02-10 18:29:41 +0530

    changes

---
+++ corejava/com/shyam/core/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.shyam.core;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean a=false;
+		
+		boolean f=a?true:false;
+		System.out.println("the boolean value "+f);
+
+	}
+
+}
